5e5a1f28a8bd1dc1a88226c26c1fd451cd1f02a3
Alxjlv <alxjlv@gmail.com>
2019-07-05 00:03:30 +1200

    Changes styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div className="header">
-            <h1 className='headerText'>How Old? 🤔</h1>
+            <h1 className='headerText'>🔥🔥How Old? 🤔🤑👌🔥🔥</h1>
         </div>
     )
 }
